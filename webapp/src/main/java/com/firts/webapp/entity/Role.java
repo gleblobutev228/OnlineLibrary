fb5ae1ad3a9b6f4c485da1d70407c137b926491e
Dmitriy Demchuk <smildemchuk130206@gmail.com>
2026-03-09 15:44:11 +0900

    add user entity with roles

--- webapp/src/main/java/com/firts/webapp/entity/Role.java
+++ webapp/src/main/java/com/firts/webapp/entity/Role.java
@@ -1,4 +1,12 @@
 package com.firts.webapp.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Role {
+    @Id
+    private String name;
 }
--- webapp/src/main/java/com/firts/webapp/entity/User.java
+++ webapp/src/main/java/com/firts/webapp/entity/User.java
@@ -1,4 +1,23 @@
 package com.firts.webapp.entity;
 
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
 }
